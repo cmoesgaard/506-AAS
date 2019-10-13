7e5c9b62fe86ed2e7de0d7ca51e4ed041a3eac60
Christoffer Moesgaard <cmoesgaard@gmail.com>
2019-10-13 14:15:40 +0200

    Streamline repo and remove old unused code
    Added simple "talk" function for the bot
    Add various comments

--- bot.py
+++ bot.py
@@ -1,93 +1,41 @@
+import argparse
 import configparser
-import datetime
 import os
 
 import requests
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
-config.read(os.path.join(base_path, 'config.ini'))
+config.read(os.path.join(base_path, "config.ini"))
 
-rocket_config = config['ROCKET']
-
-MENU_URL = config['MENU']['URL']
-HOOK_URL = rocket_config['URL']
-ICON_URL = rocket_config['ICON']
-CHANNEL = rocket_config['CHANNEL']
-BOT_NAME = rocket_config['BOT_NAME']
-
-
-def get_menu() -> dict:
-    r = requests.get(MENU_URL)
-    r.raise_for_status()
-    return r.json()
-
-
-def get_cake() -> dict:
-    r = requests.get(f'{MENU_URL}/cake')
-    r.raise_for_status()
-    return r.json()
-
-
-def prettify(text):
-    return "\n".join(text)
-
-
-def format_menu(menu: dict) -> str:
-    today = datetime.date.today()
-
-    menu_text = f"""*Menu for {today}*:
-{menu['blurb']}
-
-*Dish of the day*
-{prettify(menu['dish_of_the_day'])}
-*Eat with*
-{prettify(menu['eat_with'])}"""
-
-    if menu.get('top_with'):
-        menu_text += f"""
-*Top with*
-{prettify(menu['top_with'])}"""
-
-    menu_text += f"""
-
-*The other stuff*
-{prettify(menu['the_other_stuff'])}"""
-
-    return menu_text
-
-
-def format_cake(cake: dict) -> str:
-    cake_text = f"""
-    *Cake*
-    {cake['cake']}
-    """
-    return cake_text
+rocket_config = config["ROCKET"]
+HOOK_URL = rocket_config["URL"]
+ICON_URL = rocket_config["ICON"]
+CHANNEL = rocket_config["CHANNEL"]
+BOT_NAME = rocket_config["BOT_NAME"]
 
 
 def post(text: str):
-    r = requests.post(HOOK_URL, json={
-        'text': text,
-        'channel': CHANNEL,
-        'from': BOT_NAME,
-        'icon_url': ICON_URL
-    })
+    """
+    Post the menu to the configured destination
+    :param text: A prettified menu
+    """
+    r = requests.post(
+        HOOK_URL,
+        json={"text": text, "channel": CHANNEL, "from": BOT_NAME, "icon_url": ICON_URL},
+    )
     r.raise_for_status()
 
 
-def main():
-    menu = get_menu()
-    menu_text = format_menu(menu)
-
-    weekday = datetime.date.today().isoweekday()
-    # If Thursday
-    if weekday == 4:
-        cake = get_cake()
-        cake_text = format_cake(cake)
-        menu_text += cake_text
-
-    post(menu_text)
+def talk():
+    """ Gives you a simple prompt to write messages as the bot """
+    try:
+        while True:
+            text = input()
+            post(text)
+    except KeyboardInterrupt:
+        pass
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    talk()
---
+++ menu.py
@@ -0,0 +1,150 @@
+import configparser
+import os
+from typing import List
+
+import requests
+import re
+import datetime
+import html
+
+from bot import post
+
+ALLERGEN_MAP = {
+    "sulfur-dioxide-sulfites": None,
+    "mushrooms": None,
+    "celery": None,
+    "eggs": None,
+    "gluten": ":bread:",
+    "mustard": None,
+    "sesame-seeds": None,
+    "milk-lactose": ":milk:",
+    "soy": None,
+    "fish": ":fish:",
+    "shellfish": None,
+    "nuts": ":contains-nuts:",
+    "peanuts": None,
+}
+
+
+def get_menu() -> str:
+    """
+    Get the menu of the day and return as a formatted string, ready to post
+    """
+    date = datetime.date.today()
+    urls = generate_urls(date)
+    menu_json = fetch_menu(urls)
+    menu = extract_menu(menu_json, date)
+
+    return menu
+
+
+def fetch_menu(urls: List[str]) -> dict:
+    """
+    From a list of URLs, return the first menu found if any
+    raise Exception otherwise
+    :param urls: A list of URL strings
+    """
+    for url in urls:
+        try:
+            r = requests.get(url)
+            menu_json = r.json()
+            if menu_json:
+                return menu_json
+        except:
+            pass
+    raise Exception("No menu found")
+
+
+def extract_menu(menu_json: dict, date: datetime.date) -> str:
+    """
+    Convert a menu dict to a prettified menu string
+    :param menu_json: A menu dict from the API
+    :param date: The date corresponding to the menu
+    """
+
+    inner_menu = menu_json[-1]
+    acf = inner_menu.get("acf")
+
+    date_string = f"*Menu for {date}*"
+    story = prettify(acf.get("story")).strip()
+    menu_items = parse_menu_items(acf.get("menu_items"))
+
+    return "\n".join([date_string] + [story] + menu_items)
+
+
+def parse_menu_items(menu_items: List[dict]) -> List[str]:
+    """
+    Parse and prettify a list of menu items
+    :param menu_items: A list of dicts containing the menu from the API
+    """
+
+    def parse_menu_item(menu_item: dict) -> str:
+        text = menu_item.get("text").strip()
+
+        allergens = menu_item.get("allergens")
+        allergen_emojis = []
+        if allergens:
+            for allergen in allergens:
+                allergen_key = allergen.get("slug")
+                emoji = ALLERGEN_MAP.get(allergen_key)
+                if emoji:
+                    allergen_emojis.append(emoji)
+
+        return f'{text} {" ".join(allergen_emojis)}'.strip()
+
+    handlers = {
+        "menu_title": lambda text: f"\n*{text}*",
+        "menu_description": lambda text: f"*{text}*",
+        "menu_item": parse_menu_item,
+    }
+
+    parsed_items = []
+
+    for item in menu_items:
+        # acf_fc_layout conveniently tells us how to format each row
+        item_type = item.get("acf_fc_layout")
+        item_text = item.get(item_type)
+        parsed_item = prettify(handlers[item_type](item_text))
+        parsed_items.append(parsed_item)
+
+    return parsed_items
+
+
+def prettify(item: str) -> str:
+    """
+    Strip HTML tags and unescape any characters
+    :param item: A string to be prettified
+    """
+    item = re.sub("<.+?>", "", item)
+    return html.unescape(item)
+
+
+def generate_urls(date: datetime.date) -> List[str]:
+    """
+    Generate the possible URLs for the menu of a given day
+    :param date: The date to generate URLs for
+    :return: A list of possible URLs
+    """
+    date_string = date.strftime("%B-%-d-%Y").lower()
+    url_tday = (
+        f"https://trouble.tools/506/wp-json/wp/v2/multiple-post-type"
+        f"?slug={date_string}-thoughtful-t-day&type[]=page&type[]=topic&type[]=story&"
+        f"type[]=product&type[]=collection&type[]=event&type[]=menu&"
+        f"type[]=person&type[]=recipe"
+    )
+    url = (
+        f"https://trouble.tools/506/wp-json/wp/v2/multiple-post-type"
+        f"?slug={date_string}&type[]=page&type[]=topic&type[]=story&"
+        f"type[]=product&type[]=collection&type[]=event&type[]=menu&"
+        f"type[]=person&type[]=recipe"
+    )
+    return [url_tday, url]
+
+
+def main():
+    menu = get_menu()
+    post(menu)
+
+
+if __name__ == "__main__":
+    main()
